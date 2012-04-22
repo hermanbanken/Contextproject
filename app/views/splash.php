6e67e1f4d40390d5f60109fef617f8c82401b63d
Herman Banken <hermanbanken@gmail.com>
2012-04-23 01:15:56 +0200

    Link to map from splashpage
    Signed-off-by: Herman Banken <hermanbanken@gmail.com>

--- app/views/splash.php
+++ app/views/splash.php
@@ -13,6 +13,6 @@
 			<h1><img alt="CultuurApp" src="<?php echo URL::site('images/logo.png'); ?>"</h1>
 			<p style="color:black;">Find monuments in the Netherlands</p>
 			<p class="download-info">
-				<a href="<?php echo URL::site('monument'); ?>" class="btn btn-primary btn-large">Bekijk kaart</a>
+				<a href="<?php echo URL::site('monument/map'); ?>" class="btn btn-primary btn-large">Bekijk kaart</a>
 				<a href="<?php echo URL::site(''); ?>" class="btn btn-large">Maak een account <small>(gratis)</small></a></p></div><div class="bs-links"></div>
 	</header>
