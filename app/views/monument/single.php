2c492df757b389d794ab0ff622cd1f6827d98f46
Sjoerd van Bekhoven <svanbekhoven@gmail.com>
2012-04-22 21:47:28 +0200

    updated layout and single

--- app/views/monument/single.php
+++ app/views/monument/single.php
@@ -2,3 +2,6 @@
             <h1><?php echo $monument->name; ?></h1>
             <p class="lead">Lalala</p>
     </header>
+    <div class="span12">
+    	<img style="width: 200px;" src="/public/photos/<?php echo $monument->id_monument; ?>.JPG" alt="<?php echo $monument->name; ?>" />
+    </div>
