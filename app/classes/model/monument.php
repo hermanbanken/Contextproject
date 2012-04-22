2ba63bba1d49d122674678ea4a08357b959bdc0e
Sjoerd van Bekhoven <svanbekhoven@gmail.com>
2012-04-22 14:49:12 +0200

    updated monument model

--- app/classes/model/monument.php
+++ app/classes/model/monument.php
@@ -13,6 +13,8 @@ class Model_Monument extends Model_Abstract_Cultuurorm {
 	protected static $entity = "monument";
 	protected static $schema_sql = "CREATE TABLE IF NOT EXISTS `%s` (
 	  	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	  	`title` varchar(80) NOT NULL,
+	  	`wiki_url` varchar(200) NOT NULL,
 	  	`province` varchar(12) NOT NULL,
 	  	`municipality` varchar(50) NOT NULL,
 	  	`town` varchar(15) NOT NULL,
@@ -21,8 +23,8 @@ class Model_Monument extends Model_Abstract_Cultuurorm {
 	  	`zipCode` varchar(7) NOT NULL,
 	  	`function` varchar(50) NULL,
 	  	`description` text NULL,
-	  	`x` double(10, 5) NULL,
-	  	`y` double(10, 5) NULL,
+	  	`lng` double(10, 5) NULL,
+	  	`lat` double(10, 5) NULL,
 	  	PRIMARY KEY (`id`)
 	  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";	
 }
