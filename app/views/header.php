dcd8682f31c080fd3d7673cc0b451abe4fac58fa
Herman Banken <hermanbanken@gmail.com>
2012-05-06 20:03:58 +0200

    Made Apple touch icons and startup image
    Signed-off-by: Herman Banken <hermanbanken@gmail.com>

--- app/views/header.php
+++ app/views/header.php
@@ -1,2 +1,23 @@
 <?php echo $css; ?>
 <?php echo $js_head; ?>
+
+<meta name="description" content="Vind monumenten in heel het land" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+ 
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<link rel="apple-touch-startup-image" href="images/startup-full.jpg">
+<link rel="apple-touch-icon" href="images/touch-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="images/touch-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="images/touch-icon-114x114.png" />
+
+<!-- Le styles -->
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
