bc4872898498bb9ed5fae6ad438e3528a5c65232
Rutger Plak <rsplak@gmail.com>
2012-05-13 20:53:32 +0200

    view for tagcloud

---
+++ app/views/monument/tagcloud.php
@@ -0,0 +1,6 @@
+
+<?php
+    foreach($tags as $key=>$tag) {
+    echo "<a style='font-size: ".$tag['fontsize']."px;' href='".URL::site("monument/list")."/".$tag['content']."'>".$tag['content']."</a>    ";
+    }
+?>
