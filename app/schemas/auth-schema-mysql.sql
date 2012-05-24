c7d476b6bef47a3a7459222c95de3155caa857cc
Herman Banken <hermanbanken@gmail.com>
2012-05-24 11:11:41 +0200

    Fix reference-error when creating role table
    Signed-off-by: Herman Banken <hermanbanken@gmail.com>

--- app/schemas/auth-schema-mysql.sql
+++ app/schemas/auth-schema-mysql.sql
@@ -11,15 +11,6 @@ CREATE TABLE IF NOT EXISTS `%sroles` (
 REPLACE INTO `%sroles` (`id`, `name`, `description`) VALUES(1, 'login', 'Login privileges, granted after account confirmation');
 REPLACE INTO `%sroles` (`id`, `name`, `description`) VALUES(2, 'admin', 'Administrative user, has access to everything.');
 
-CREATE TABLE IF NOT EXISTS `%sroles_users` (
-  `user_id` int(10) UNSIGNED NOT NULL,
-  `role_id` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY  (`user_id`,`role_id`),
-  KEY `fk_role_id` (`role_id`),
-  CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `%susers` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `%sroles` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE IF NOT EXISTS `%susers` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `email` varchar(254) NOT NULL,
@@ -32,6 +23,15 @@ CREATE TABLE IF NOT EXISTS `%susers` (
   UNIQUE KEY `uniq_email` (`email`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `%sroles_users` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `role_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY  (`user_id`,`role_id`),
+  KEY `fk_role_id` (`role_id`),
+  CONSTRAINT `%sroles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `%susers` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `%sroles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `%sroles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `%suser_tokens` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(11) UNSIGNED NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `%suser_tokens` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `uniq_token` (`token`),
   KEY `fk_user_id` (`user_id`),
-  CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `%susers` (`id`) ON DELETE CASCADE
+  CONSTRAINT `%suser_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `%susers` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
