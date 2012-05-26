0e47806e3835fac85a2cd8a35676b04741fc51a2
Herman Banken <hermanbanken@gmail.com>
2012-05-26 16:16:54 +0200

    Added Pikik Analytics tracker

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,1 +1,16 @@
-<?php echo $js_foot; ?>
+<?php echo $js_foot;
+
+if(Kohana::$environment == Kohana::PRODUCTION){ ?>
+<!-- Piwik -->
+<script type="text/javascript">
+    var pkBaseURL = (("https:" == document.location.protocol) ? "https://clients.hermanbanken.nl/piwik/" : "http://clients.hermanbanken.nl/piwik/");
+    document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+    try {
+        var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 7);
+        piwikTracker.trackPageView();
+        piwikTracker.enableLinkTracking();
+    } catch( err ) {}
+</script><noscript><p><img src="http://clients.hermanbanken.nl/piwik/piwik.php?idsite=7" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
+<?php } ?>
