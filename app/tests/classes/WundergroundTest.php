d2a8e617a3e6768e861a15934d959ef98c8246b0
Tim Eversdijk <TimEversdijk@gmail.com>
2012-06-04 14:01:40 +0200

    MonumentTest TextualMagicTest XMLparserTest WundergroundTest

---
+++ app/tests/classes/WundergroundTest.php
@@ -0,0 +1,20 @@
+<?php
+class WundergroundTest extends Kohana_UnitTest_TestCase
+{
+	public function test_forecast(){
+	
+		$fakemonument = ORM::factory('monument');
+		$fakemonument->id_town = 72;
+		$fakemonument->lng = 52;
+		$fakemonument->lat = 4.37;
+		$fakemonument->name = "fake monument1";
+		$fakemonument->description = "Dit is fake monument 1";
+		$fakemonument->id_monument = 1;
+		
+		print_r(Wunderground::forecast($fakemonument[0])->as_array());
+		
+	}
+	
+}
+
+?>
