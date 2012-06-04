640660cae8eb6f47cf1f90a89e18bbde354c0575
Rutger Plak <rsplak@gmail.com>
2012-06-04 11:18:46 +0200

    error handling
    Signed-off-by: Rutger Plak <rsplak@gmail.com>

--- app/classes/kohana/exception.php
+++ app/classes/kohana/exception.php
@@ -3,7 +3,7 @@ class Kohana_Exception extends Kohana_Kohana_Exception {
 
 	public static function handler(Exception $e)
 	{
-		if (false && (Kohana::DEVELOPMENT === Kohana::$environment || Kohana::STAGING === Kohana::$environment))
+		if (Kohana::DEVELOPMENT === Kohana::$environment || Kohana::STAGING === Kohana::$environment)
 		{
 			parent::handler($e);
 		}
